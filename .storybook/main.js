13c05bb8e6a56a96b198532fca8db6fa52415975
Allan <allan.s.bond@gmail.com>
2020-07-07 09:15:24 -0400

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../projects/my-app/**/*.stories.(js|mdx)"],
+  addons: ["@storybook/addon-docs"],
+};
---
+++ projects/my-app/src/app/app.stories.js
@@ -0,0 +1,9 @@
+import { html } from "lit-html";
+
+// import '../my-component.js';
+
+export default {
+  title: "App",
+};
+
+export const Heading = () => html` <h1>Hello World</h1> `;
